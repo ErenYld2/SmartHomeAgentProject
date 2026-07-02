9479b2e701bba9acb38b0e6c2181edd27e3fbdc5
ErenYld2 <cav1t3ren@gmail.com>
2026-07-02 12:03:27 +0300

    Update config.py

--- SmartHomeAgentProject/config.py
+++ SmartHomeAgentProject/config.py
@@ -1,2 +1,2 @@
-API_KEY = "AIzaSyBMKpjn-vGeMhWFnIw2SekGDkPOFkNsZGU"  # Buraya kendi Gemini API anahtarınızı yazın
+API_KEY = "YourKEY"  # Buraya kendi Gemini API anahtarınızı yazın
 MODEL_NAME = "gemini-1.5-flash"
